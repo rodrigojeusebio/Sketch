feed29fc6f6d993ea3c5088deed2e2a3d784d194
Rodrigo Eusebio <rodrigojeusebio@gmail.com>
2021-11-30 17:36:51 +0000

    New functions implemented

--- index.js
+++ index.js
@@ -1,37 +1,61 @@
 const container = document.querySelector('.container')
 const buttons = document.querySelectorAll('button')
+
 const black = "#190707";
-const clear = "rgba(0, 0, 0, .1)";
+const background_Color = "rgba(0, 0, 0, .1)";
 let main_color = black;
+let random = false
 
-function creatDivs(col , rows) {
-    for(let i = 0;i < (col * rows); i++) {
-        const div = document.createElement('div') 
-        container.style.gridTemplateColumns = `repeat(${col}, 1fr)`;
-        container.style.gridTemplateRows = `repeat(${rows}, 1fr)`;
-        container.appendChild(div).classList.add('box')
+
+//Manage divs
+function creatDivs(size) {
+    for(let i = 0;i < (size * size); i++) {
+        const div = document.createElement('div') ;
+        container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+        container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+        container.appendChild(div).classList.add('box');
     }
-}
-creatDivs(16,16)
-
-const boxes = document.querySelectorAll('.box')
-
-boxes.forEach(box => {
+    boxes = document.querySelectorAll(".box")
+    boxes.forEach(box => {
     box.addEventListener("mouseenter", function(){
         box.style.backgroundColor = main_color;
+        if(random == true){
+            main_color = `rgb(${Math.floor(Math.random()*256)},${Math.floor(Math.random()*256)},${Math.floor(Math.random()*256)})`
+            console.log(main_color)
+        }
     });
 });
+}
 
+function clearDivs(){
+    container.innerHTML = "";
+}
 
 buttons.forEach(button => {
-    button.addEventListener("click", function(){
+    button.addEventListener("click",function(){
         if (button.classList.value == "clear"){
             boxes.forEach(box => {
-                box.style.backgroundColor = clear;
+                box.style.backgroundColor = background_Color;
+                main_color = black;
             });
+        }else if (button.classList.value == "eraser"){
+            random = false;
+            main_color = background_Color;
+        }else if (button.classList.value == "black"){
+            main_color = black;
+            random = false;
+        }else if (button.classList.value == "random_col"){
+            random = true;
+        }else if (button.classList.value == "new_grid"){
+            let val = parseInt(prompt("grid size"));
+            keep = false
+            clearDivs()
+            creatDivs(val)
         }
-        if (button.classList.value == "eraser"){
-            main_color = clear;
-        }
-    });
+    })
 });
+
+
+window.onload = () => {
+    creatDivs(16)
+  }
