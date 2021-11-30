d2480a68e55f37a4c757d51d19608269c9670afa
Rodrigo <rodrigojeusebio@gmail.com>
2021-11-30 14:21:40 +0000

    Now we have buttons

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const container = document.querySelector('.container')
-let color = black;
+const buttons = document.querySelectorAll('button')
+const black = "#190707";
+const clear = "rgba(0, 0, 0, .1)";
+let main_color = black;
 
 function creatDivs(col , rows) {
     for(let i = 0;i < (col * rows); i++) {
@@ -14,9 +17,21 @@ creatDivs(16,16)
 const boxes = document.querySelectorAll('.box')
 
 boxes.forEach(box => {
-    box.addEventListener("mouseout", function(){
-        box.style.backgroundColor = "red";
-        console.log("hello");
+    box.addEventListener("mouseenter", function(){
+        box.style.backgroundColor = main_color;
     });
 });
 
+
+buttons.forEach(button => {
+    button.addEventListener("click", function(){
+        if (button.classList.value == "clear"){
+            boxes.forEach(box => {
+                box.style.backgroundColor = clear;
+            });
+        }
+        if (button.classList.value == "eraser"){
+            main_color = clear;
+        }
+    });
+});
