c954915cba1b2528ee55248d52375cc7acd1f850
Rodrigo Eusébio <rodrigojeusebio@gmail.com>
2025-09-07 12:00:53 +0100

    Improve UI

--- index.js
+++ index.js
@@ -1,78 +1,125 @@
+const BLACK = "#190707";
+
+randomColoror = () => Math.floor(Math.random() * 256);
+
+class Canvas {
+    mouseDown;
+    randomColor = false;
+    backgroundColor = "rgba(255, 255, 255, 0.747)";
+    mainColor = BLACK;
+    boxes;
+
+    constructor(container, buttons) {
+        this.container = container;
+        this.buttons = buttons;
+        this.mouseDown = false;
+    };
+
+    creatDivs(size) {
+        for (let i = 0; i < (size * size); i++) {
+            const div = document.createElement('div');
+            this.container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+            this.container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+            this.container.appendChild(div).classList.add('box');
+        }
+
+        this.boxes = document.querySelectorAll(".box");
+
+        this.boxes.forEach(box => {
+            box.addEventListener("mouseenter", () => {
+                if (!this.mouseDown) {
+                    return;
+                }
+
+                box.style.backgroundColor = this.mainColor;
+                if (this.randomColor == true) {
+                    this.mainColor = `rgb(${randomColoror()},${randomColoror()},${randomColoror()})`
+                }
+            });
+        });
+    }
+
+    clearDivs() {
+        this.container.innerHTML = "";
+    }
+
+    colorActiveButton(buttonIdentifier) {
+        let button = document.querySelector(`#${buttonIdentifier}`);
+        button.style.backgroundColor = '#8ea7a5ff';
+
+        let buttons = document.querySelectorAll('.options')
+        buttons.forEach((buttonOption) => {
+            if (button == buttonOption) {
+                return;
+            }
+            buttonOption.style.backgroundColor = '';
+        })
+    }
+
+    setEraser() {
+        this.randomColor = false;
+        this.mainColor = this.backgroundColor;
+        this.colorActiveButton('eraser')
+    }
+
+    setBlack() {
+        this.mainColor = BLACK;
+        this.randomColor = false;
+        this.colorActiveButton('black')
+    }
+
+    setRandomColor() {
+        this.randomColor = true;
+        this.colorActiveButton('randomColor')
+    }
+
+    setClear() {
+        this.boxes.forEach(box => {
+            box.style.backgroundColor = this.backgroundColor;
+        });
+    }
+
+    initEventListener() {
+        this.buttons.forEach(button => {
+            button.addEventListener("click", () => {
+                if (button.id == "clear") {
+                    this.setClear()
+                } else if (button.id == "eraser") {
+                    this.setEraser()
+                } else if (button.id == "black") {
+                    this.setBlack()
+                } else if (button.id == "randomColor") {
+                    this.setRandomColor()
+                } else if (button.id == "new_grid") {
+                    let val = parseInt(prompt("Grid size (Between 1 and 200)"));
+                    if (val > 200 || val < 1 || val === NaN) {
+                        this.clearDivs()
+                        this.creatDivs(16)
+                    } else {
+                        this.clearDivs()
+                        this.creatDivs(val)
+                    }
+                }
+            })
+        });
+    }
+}
+
 const container = document.querySelector('.container')
 const buttons = document.querySelectorAll('button')
 
-const BLACK = "#190707";
-let backgroundColor = "rgba(255, 255, 255, 0.747)";
-let main_color = BLACK;
-let random = false
-
-random_color = () => Math.floor(Math.random() * 256);
-
-//Manage divs
-function creatDivs(size) {
-    for (let i = 0; i < (size * size); i++) {
-        const div = document.createElement('div');
-        container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-        container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
-        container.appendChild(div).classList.add('box');
-    }
-    boxes = document.querySelectorAll(".box")
-    boxes.forEach(box => {
-        box.addEventListener("mouseenter", function () {
-            if (!mouseDown) {
-                return;
-            }
-
-            box.style.backgroundColor = main_color;
-            if (random == true) {
-                main_color = `rgb(${random_color()},${random_color()},${random_color()})`
-            }
-        });
-    });
-}
-
-function clearDivs() {
-    container.innerHTML = "";
-}
-
-buttons.forEach(button => {
-    button.addEventListener("click", function () {
-        if (button.classList.value == "clear") {
-            boxes.forEach(box => {
-                box.style.backgroundColor = backgroundColor;
-                main_color = BLACK;
-            });
-        } else if (button.classList.value == "eraser") {
-            random = false;
-            main_color = backgroundColor;
-        } else if (button.classList.value == "black") {
-            main_color = BLACK;
-            random = false;
-        } else if (button.classList.value == "random_col") {
-            random = true;
-        } else if (button.classList.value == "new_grid") {
-            let val = parseInt(prompt("grid size"));
-            console.log(val)
-            if (val > 100 || val < 1 || val === NaN) {
-                clearDivs()
-                creatDivs(16)
-            } else {
-                clearDivs()
-                creatDivs(val)
-            }
-
-        }
-    })
-});
+const canvas = new Canvas(container, buttons);
+canvas.initEventListener();
 
 window.onmousedown = () => {
-    mouseDown = true;
+    canvas.mouseDown = true;
+    console.log(canvas.mouseDown)
 }
 
 window.onmouseup = () => {
-    mouseDown = false;
+    canvas.mouseDown = false;
 }
 
 window.onload = () => {
-    creatDivs(16)
+    canvas.creatDivs(16)
 }
