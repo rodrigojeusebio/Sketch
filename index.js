af6549398478e980ef5c5e1a2fbd20db693fd65b
Rodrigo Eusébio <rodrigojeusebio@gmail.com>
2025-09-07 11:03:42 +0100

    Only paint on click

--- index.js
+++ index.js
@@ -1,67 +1,78 @@
 const container = document.querySelector('.container')
 const buttons = document.querySelectorAll('button')
 
-const black = "#190707";
-const background_Color = "rgba(255, 255, 255, 0.747)";
-let main_color = black;
+const BLACK = "#190707";
+let backgroundColor = "rgba(255, 255, 255, 0.747)";
+let main_color = BLACK;
 let random = false
 
+random_color = () => Math.floor(Math.random() * 256);
 
 //Manage divs
 function creatDivs(size) {
-    for(let i = 0;i < (size * size); i++) {
-        const div = document.createElement('div') ;
+    for (let i = 0; i < (size * size); i++) {
+        const div = document.createElement('div');
         container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
         container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
         container.appendChild(div).classList.add('box');
     }
     boxes = document.querySelectorAll(".box")
     boxes.forEach(box => {
-    box.addEventListener("mouseenter", function(){
-        box.style.backgroundColor = main_color;
-        if(random == true){
-            main_color = `rgb(${Math.floor(Math.random()*256)},${Math.floor(Math.random()*256)},${Math.floor(Math.random()*256)})`
-            console.log(main_color)
-        }
+        box.addEventListener("mouseenter", function () {
+            if (!mouseDown) {
+                return;
+            }
+
+            box.style.backgroundColor = main_color;
+            if (random == true) {
+                main_color = `rgb(${random_color()},${random_color()},${random_color()})`
+            }
+        });
     });
-});
 }
 
-function clearDivs(){
+function clearDivs() {
     container.innerHTML = "";
 }
 
 buttons.forEach(button => {
-    button.addEventListener("click",function(){
-        if (button.classList.value == "clear"){
+    button.addEventListener("click", function () {
+        if (button.classList.value == "clear") {
             boxes.forEach(box => {
-                box.style.backgroundColor = background_Color;
-                main_color = black;
+                box.style.backgroundColor = backgroundColor;
+                main_color = BLACK;
             });
-        }else if (button.classList.value == "eraser"){
+        } else if (button.classList.value == "eraser") {
             random = false;
-            main_color = background_Color;
-        }else if (button.classList.value == "black"){
-            main_color = black;
+            main_color = backgroundColor;
+        } else if (button.classList.value == "black") {
+            main_color = BLACK;
             random = false;
-        }else if (button.classList.value == "random_col"){
+        } else if (button.classList.value == "random_col") {
             random = true;
-        }else if (button.classList.value == "new_grid"){
+        } else if (button.classList.value == "new_grid") {
             let val = parseInt(prompt("grid size"));
             console.log(val)
-            if (val > 100 || val < 1 || val === NaN){
+            if (val > 100 || val < 1 || val === NaN) {
                 clearDivs()
                 creatDivs(16)
-            } else{
+            } else {
                 clearDivs()
                 creatDivs(val)
             }
-            
+
         }
     })
 });
 
+window.onmousedown = () => {
+    mouseDown = true;
+}
+
+window.onmouseup = () => {
+    mouseDown = false;
+}
 
 window.onload = () => {
     creatDivs(16)
-  }
+}
